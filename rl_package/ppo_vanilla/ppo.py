f6721bd995253ab4df36080dd1bf7eaf1f29e81f
Atish Dixit <atishdixit16@gmail.com>
2020-09-22 00:32:56 +0100

    add network initialization  parameter in PPO

--- rl_package/ppo_vanilla/ppo.py
+++ rl_package/ppo_vanilla/ppo.py
@@ -27,6 +27,57 @@ def init_weights(m):
         nn.init.normal_(m.weight, mean=0., std=0.1)
         nn.init.constant_(m.bias, 0.1)
 
+def initialize_weights(mod, initialization_type, scale=1.4142):
+    '''
+    Weight initializer for the models.
+    Inputs: A model, Returns: none, initializes the parameters
+    '''
+    for p in mod.parameters():
+        if initialization_type == "normal":
+            nn.init.normal_(p.data, mean=0., std=0.1)
+        elif initialization_type == "xavier":
+            if len(p.data.shape) >= 2:
+                nn.init.xavier_uniform_(p.data)
+            else:
+                p.data.zero_()
+        elif initialization_type == "orthogonal":
+            if len(p.data.shape) >= 2:
+                orthogonal_init(p.data, gain=scale)
+            else:
+                p.data.zero_()
+        else:
+            raise ValueError("Need a valid initialization key")
+
+def orthogonal_init(tensor, gain=1):
+    '''
+    Fills the input `Tensor` using the orthogonal initialization scheme from OpenAI
+    Args:
+        tensor: an n-dimensional `torch.Tensor`, where :math:`n \geq 2`
+        gain: optional scaling factor
+    Examples:
+        >>> w = torch.empty(3, 5)
+        >>> orthogonal_init(w)
+    '''
+    if tensor.ndimension() < 2:
+        raise ValueError("Only tensors with 2 or more dimensions are supported")
+
+    rows = tensor.size(0)
+    cols = tensor[0].numel()
+    flattened = tensor.new(rows, cols).normal_(0, 1)
+
+    if rows < cols:
+        flattened.t_()
+
+    # Compute the qr factorization
+    u, s, v = torch.svd(flattened, some=True)
+    if rows < cols:
+        u.t_()
+    q = u if tuple(u.shape) == (rows, cols) else v
+    with torch.no_grad():
+        tensor.view_as(q).copy_(q)
+        tensor.mul_(gain)
+    return tensor
+
 def get_activation(activation):
     if activation=='relu':
         return nn.ReLU()
@@ -37,21 +88,25 @@ def get_activation(activation):
     else:
         raise Exception('invalid activation key. should be one of these: relu, tanh or sigmoid')
 
-def get_moduledict(num_inputs, num_outputs, MLP_LAYERS, MLP_ACTIVATIONS, ACTOR_FINAL_ACTIVATION, network_key):
+def get_moduledict(num_inputs, num_outputs, MLP_LAYERS, MLP_ACTIVATIONS, ACTOR_FINAL_ACTIVATION, NN_INIT, network_key):
     module_list = {}
     for layer, activation, i in zip( MLP_LAYERS, MLP_ACTIVATIONS, range(len(MLP_LAYERS)) ):
         if i==0:
             module_list['layer '+str(i)] = nn.Linear(num_inputs, layer)
+            initialize_weights( module_list['layer '+str(i)] , NN_INIT)
             module_list['layer '+str(i)+' act'] = get_activation(activation)
             last_layer = layer
         else:
             module_list['layer '+str(i)] = nn.Linear(last_layer, layer)
+            initialize_weights( module_list['layer '+str(i)] , NN_INIT)
             module_list['layer '+str(i)+' act'] = get_activation(activation)
             last_layer = layer
     if network_key=='actor':
         module_list['layer '+str(i+1)] = nn.Linear(last_layer, num_outputs)
+        initialize_weights( module_list['layer '+str(i+1)] , NN_INIT, scale=1.0)
     elif network_key=='critic':
         module_list['layer '+str(i+1)] = nn.Linear(last_layer, 1)
+        initialize_weights( module_list['layer '+str(i+1)] , NN_INIT, scale=1.0)
     else:
         raise Exception('invalid network key. should be one of these: actor or critic')
     
@@ -63,14 +118,14 @@ def get_moduledict(num_inputs, num_outputs, MLP_LAYERS, MLP_ACTIVATIONS, ACTOR_F
 
 
 class ActorCritic(nn.Module):
-    def __init__(self, num_inputs, num_outputs, MLP_LAYERS, MLP_ACTIVATIONS, ACTOR_FINAL_ACTIVATION, std=0.0):
+    def __init__(self, num_inputs, num_outputs, MLP_LAYERS, MLP_ACTIVATIONS, ACTOR_FINAL_ACTIVATION, NN_INIT, std=0.0):
         super(ActorCritic, self).__init__()
 
-        self.critic = nn.Sequential ( OrderedDict (get_moduledict(num_inputs, num_outputs, MLP_LAYERS, MLP_ACTIVATIONS, ACTOR_FINAL_ACTIVATION, 'critic') )  )
-        self.actor = nn.Sequential ( OrderedDict (get_moduledict(num_inputs, num_outputs, MLP_LAYERS, MLP_ACTIVATIONS, ACTOR_FINAL_ACTIVATION, 'actor') ) )
+        self.critic = nn.Sequential ( OrderedDict (get_moduledict(num_inputs, num_outputs, MLP_LAYERS, MLP_ACTIVATIONS, ACTOR_FINAL_ACTIVATION, NN_INIT, 'critic') )  )
+        self.actor = nn.Sequential ( OrderedDict (get_moduledict(num_inputs, num_outputs, MLP_LAYERS, MLP_ACTIVATIONS, ACTOR_FINAL_ACTIVATION, NN_INIT, 'actor') ) )
         self.log_std = nn.Parameter(torch.ones(1, num_outputs) * std)
         
-        self.apply(init_weights)
+        # self.apply(init_weights)
         
     def forward(self, x):
         value = self.critic(x)
@@ -139,10 +194,11 @@ def ppo_update(ppo_epochs, mini_batch_size, states, actions, log_probs, returns,
 
 
 def ppo_algorithm(ENV, NUM_ENV=8,
-                  TOTAL_STEPS=240000, NSTEPS=64, MINIBATCH_SIZE=128, N_EPOCH=30,
+                  TOTAL_STEPS=200000, NSTEPS=64, MINIBATCH_SIZE=128, N_EPOCH=30,
                   CLIP_PARAM=0.1, VF_COEF=0.5, ENT_COEF=0.001,
                   GAMMA=0.99, LAMBDA=0.95,
-                  MLP_LAYERS=[64,64], MLP_ACTIVATIONS=['relu', 'relu'], GRAD_CLIP=False, LR_ANNEAL=False, ACTOR_FINAL_ACTIVATION=None, ACTOR_DIST_LOG_STD=0.0, LEARNING_RATE=1e-3,
+                  MLP_LAYERS=[64,64], MLP_ACTIVATIONS=['relu', 'relu'], ACTOR_FINAL_ACTIVATION=None, ACTOR_DIST_LOG_STD=0.0, LEARNING_RATE=1e-3,
+                  GRAD_CLIP=False, LR_ANNEAL=False, NN_INIT='normal',
                   PRINT_FREQ=8000, N_TEST_ENV=50, TEST_ENV_FUNC=test_env,
                   SAVE_RESULTS=False, FILE_PATH='results/', LOG_FILE_NAME='log', SAVE_MODEL=False, MODEL_FILE_NAME='model',
                   SEED=4):
@@ -210,7 +266,7 @@ def ppo_algorithm(ENV, NUM_ENV=8,
     timesteps = []
     test_rewards = []
 
-    model = ActorCritic(num_inputs, num_outputs, MLP_LAYERS, MLP_ACTIVATIONS, ACTOR_FINAL_ACTIVATION, std=ACTOR_DIST_LOG_STD).to(device)
+    model = ActorCritic(num_inputs, num_outputs, MLP_LAYERS, MLP_ACTIVATIONS, ACTOR_FINAL_ACTIVATION, NN_INIT, std=ACTOR_DIST_LOG_STD).to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
     lam = lambda steps: 1-steps/total_steps
     scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lam)
@@ -280,4 +336,4 @@ def ppo_algorithm(ENV, NUM_ENV=8,
 
 if __name__ == "__main__":
     env = gym.make('Pendulum-v0')
-    model = ppo_algorithm(env, LR_ANNEAL=True)
+    model = ppo_algorithm(env)
