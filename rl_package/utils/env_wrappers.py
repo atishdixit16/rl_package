ead7c3d142d7c539b47db41e709e5cadec27948e
Atish Dixit <atishdixit16@gmail.com>
2020-10-15 11:54:47 +0100

    add SMMwrapper for google-football environment

--- rl_package/utils/env_wrappers.py
+++ rl_package/utils/env_wrappers.py
@@ -2,6 +2,7 @@ import numpy as np
 import gym
 import collections
 import cv2
+from typing import Tuple, Dict, Any #, Union, Callable, List, Iterable, Any, Dict
 
 
 # source: openAI standard wrappers 
@@ -162,6 +163,76 @@ class ParallelEnvWrapper():
         if self.record_done.any():
             return s,r,self.record_done,i
         return s,r,d,i
+
+
+
+
+class SMMFrameProcessWrapper(gym.Wrapper):
+    """
+    Wrapper for processing frames from SMM observation wrapper from football env.
+
+    Input is (72, 96, 4), where last dim is (team 1 pos, team 2 pos, ball pos, 
+    active player pos). Range 0 -> 255.
+    Output is (72, 96, 4) as difference to last frame for all. Range -1 -> 1
+    """
+
+    def __init__(self, env: gym.Env = None,
+                 obs_shape: Tuple[int, int] = (72, 96, 4)) -> None:
+        """
+        :param env: Gym env, or None. Allowing None here is unusual,
+                    but we'll reuse the buffer functunality later in
+                    the submission, when we won't be using the gym API.
+        :param obs_shape: Expected shape of single observation.
+        """
+        if env is not None:
+            super().__init__(env)
+        self._buffer_length = 2
+        self._obs_shape = obs_shape
+        self._prepare_obs_buffer()
+
+    @staticmethod
+    def _normalise_frame(frame: np.ndarray):
+        return frame / 255.0
+
+    def _prepare_obs_buffer(self) -> None:
+        """Create buffer and preallocate with empty arrays of expected shape."""
+
+        self._obs_buffer = collections.deque(maxlen=self._buffer_length)
+
+        for _ in range(self._buffer_length):
+            self._obs_buffer.append(np.zeros(shape=self._obs_shape))
+
+    def build_buffered_obs(self) -> np.ndarray:
+        """
+        Iterate over the last dimenion, and take the difference between this obs 
+        and the last obs for each.
+        """
+        agg_buff = np.empty(self._obs_shape)
+        for f in range(self._obs_shape[-1]):
+            agg_buff[..., f] = self._obs_buffer[1][..., f] - self._obs_buffer[0][..., f]
+
+        return agg_buff
+
+    def step(self, action: int) -> Tuple[np.ndarray, float, bool, Dict[Any, Any]]:
+        """Step env, add new obs to buffer, return buffer."""
+        obs, reward, done, info = self.env.step(action)
+
+        obs = self._normalise_frame(obs)
+        self._obs_buffer.append(obs)
+
+        return self.build_buffered_obs(), reward, done, info
+
+    def reset(self) -> np.ndarray:
+        """Add initial obs to end of pre-allocated buffer.
+
+        :return: Buffered observation
+        """
+        self._prepare_obs_buffer()
+        obs = self.env.reset()
+        self._obs_buffer.append(obs)
+
+        return self.build_buffered_obs()
+
 
 # example 
 if __name__ == "__main__":
