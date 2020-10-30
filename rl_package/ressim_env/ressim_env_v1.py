a55e3d372fcae1e510cabe4cd4b7ee585bbc3d99
Atish Dixit <atishdixit16@gmail.com>
2020-10-30 20:17:15 +0000

    add assertion for action value in ressim_env_v1

--- rl_package/ressim_env/ressim_env_v1.py
+++ rl_package/ressim_env/ressim_env_v1.py
@@ -87,9 +87,9 @@ class ResSimEnv_v1():
         return [seed]
 
     def action_to_q_mapping(self, action):
-        
+
+        assert any(action<0), 'Invalid action. condition violated: all(action>0) = True'        
         # convert input array into producer/injector 
-        # action = np.clip(action,0,1)
         inj_flow = action[:self.n_inj] / np.sum(action[:self.n_inj])
         inj_flow = self.Q * inj_flow
         prod_flow = action[self.n_inj:] / np.sum(action[self.n_inj:])
